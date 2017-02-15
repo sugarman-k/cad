186ec31d717df96c5dcb1509c8d26e611b2632da
sugarman-k <sugarman2014@outlook.com>
2017-02-15 18:32:01 +0800

    test2

--- workspace/cad/src/cad/CAD.java
+++ workspace/cad/src/cad/CAD.java
@@ -1,13 +1,16 @@
 package cad;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class CAD {
 
 	public static void main(String[] args) {
 		JFrame frame= new JFrame();
+		JButton button=new JButton();
 		frame.pack();
 		frame.setVisible(true);
+		
 	}
 
 }
