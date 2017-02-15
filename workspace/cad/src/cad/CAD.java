6c247866ffd44523175651e7fbbe5054b7a07796
sugarman-k <sugarman2014@outlook.com>
2017-02-15 17:46:53 +0800

    test

---
+++ workspace/cad/src/cad/CAD.java
@@ -0,0 +1,13 @@
+package cad;
+
+import javax.swing.JFrame;
+
+public class CAD {
+
+	public static void main(String[] args) {
+		JFrame frame= new JFrame();
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
